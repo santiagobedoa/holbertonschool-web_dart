556d6f5378756028ef829fe0aba7147c4ea03e33
santiagobedoa <santiagobedoa@gmail.com>
2023-07-25 09:33:59 -0500

    Tue Jul 25 09:33:59 -05 2023

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,48 +1,23 @@
 import 'dart:convert';
-import 'dart:async';
-import 'dart:core';
+import '4-util.dart';
 
-Future<String> fetchUserOrders(String id) async {
-  var orders = {
-    "7ee9a243-01ca-47c9-aa14-0149789764c3": ["pizza", "orange"]
-  };
+Future<double> calculateTotal() async {
   try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(orders[id]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
+    double totalPrice = 0;
+    
+    final Map<String, dynamic> userData = json.decode(await fetchUserData());
+    final String userId = userData['id'];
+    final List<dynamic> userOrders = json.decode(await fetchUserOrders(userId));
 
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
-Future<String> fetchProductPrice(product) async {
-  var products = {"pizza": 20.30, "orange": 10, "water": 5, "soda": 8.5};
-  try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(products[product]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
-
-calculateTotal() async {
-    try {
-        double price = 0;
-	
-        final Map<String, dynamic> userData = json.decode(await fetchUserData());
-        final String data = userData['id'];
-        final List<dynamic> userOrder = json.decode(await fetchUserOrders(data));
-        for (int idx = 0; idx < userOrder.length; idx++) {
-            price += json.decode(await fetchProductPrice(userOrder[idx]));
-        }
-        return price;
-    } catch (err) {
-        print('error caught: $err');
-        return -1;
+    for (int idx = 0; idx < userOrders.length; idx++) {
+      double productPrice = await fetchProductPrice(userOrders[idx]);
+      totalPrice += json.decode(productPrice);
     }
+
+    return totalPrice;
+  } catch (err) {
+    print('error caught: $err');
+    return -1;
+  }
 }
+
