ac1f57220c95157bdd660e796b864d8d98b3c243
santiagobedoa <santiagobedoa@gmail.com>
2023-07-25 09:48:27 -0500

    Tue Jul 25 09:48:27 -05 2023

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
-import '4-util.dart';
+import 'dart:async';
+import 'dart:core';
 
 Future<String> fetchUserOrders(String id) async {
   var orders = {
@@ -19,7 +20,7 @@ Future<String> fetchUserData() => Future.delayed(
           '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
     );
 
-Future<String> fetchProductPrice(String product) async {
+Future<String> fetchProductPrice(product) async {
   var products = {"pizza": 20.30, "orange": 10, "water": 5, "soda": 8.5};
   try {
     return Future.delayed(
@@ -29,25 +30,19 @@ Future<String> fetchProductPrice(String product) async {
   }
 }
 
-Future<double> calculateTotal() async {
-  try {
-    double totalPrice = 0;
-    
-    final userData = await fetchUserData();
-    final Map<String, dynamic> userDataMap = json.decode(userData);
-    final String userId = userDataMap['id'];
-    final userOrdersData = await fetchUserOrders(userId);
-    final List<dynamic> userOrders = json.decode(userOrdersData);
-
-    for (var product in userOrders) {
-      final productPriceData = await fetchProductPrice(product);
-      final double productPrice = json.decode(productPriceData);
-      totalPrice += productPrice;
+calculateTotal() async {
+    try {
+        double price = 0;
+	
+        final Map<String, dynamic> userData = json.decode(await fetchUserData());
+        final String data = userData['id'];
+        final List<dynamic> userOrder = json.decode(await fetchUserOrders(data));
+        for (int idx = 0; idx < userOrder.length; idx++) {
+            price += json.decode(await fetchProductPrice(userOrder[idx]));
+        }
+        return price;
+    } catch (err) {
+        print('error caught: $err');
+        return -1;
     }
-
-    return totalPrice;
-  } catch (err) {
-    print('error caught: $err');
-    return -1;
-  }
 }
