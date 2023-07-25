bc4e5128f06dd83229c4512825c29a6e13789836
santiagobedoa <santiagobedoa@gmail.com>
2023-07-25 09:29:03 -0500

    Tue Jul 25 09:29:03 -05 2023

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,15 +1,9 @@
+import '1-util.dart';
 import 'dart:convert';
 
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
-getUserId() async {
+Future<String> getUserId() async {
   var strRepr = await fetchUserData();
-
-  var Repr  = json.decode(strRepr);
-
-  return Repr["id"];
+  var repr = json.decode(strRepr);
+  return repr["id"];
 }
+
